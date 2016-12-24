1309b9a5582c73f372340a97150fc09c3428d298
范志康 <goodman.capu@gmail.com>
2016-12-25 01:15:10 +0800

    在线测试预处理项目

---
+++ OnlineQA/OnlineQA/OnlineQA.h
@@ -0,0 +1,18 @@
+//
+//  OnlineQA.h
+//  OnlineQA
+//
+//  Created by 范志康 on 2016/12/22.
+//  Copyright © 2016年 范志康. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define RESOURCE_FOLDER @"/Users/Frank/Downloads/互联网数据挖掘/大作业/OnlineQA/OnlineQA/data/"
+#define RESULT_FOLDER @"/Users/Frank/Downloads/互联网数据挖掘/大作业/OnlineQA/OnlineQA/data/"
+
+@interface OnlineQA : NSObject
+
+- (void)start;
+
+@end
---
+++ OnlineQA/OnlineQA/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OnlineQA
+//
+//  Created by 范志康 on 2016/12/22.
+//  Copyright © 2016年 范志康. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "OnlineQA.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        [[OnlineQA new] start];
+    }
+    return 0;
+}
